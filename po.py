1150fb38125ff82ef4422e84701353294e752845
youssef azam <108946956+youssef-azam@users.noreply.github.com>
2024-03-04 11:31:53 +0200

    po.py

--- po.py
+++ po.py
@@ -3,17 +3,14 @@ import streamlit as st
 # Set page configuration
 st.set_page_config(page_title="Your App", page_icon=":rocket:", layout="wide")
 
-# Define the Power BI dashboard URLs
-daily_sales_dashboard_url = "https://app.powerbi.com/reportEmbed?reportId=03558a93-ea7b-47b7-ae02-be32bc09bd07&autoAuth=true&ctid=d1aad15a-5724-45cd-a320-5e75718fa6bd"
-overall_sales_dashboard_url = "https://app.powerbi.com/reportEmbed?reportId=035ca501-5ee6-40a6-bcd7-e07df21f67f2&autoAuth=true&ctid=d1aad15a-5724-45cd-a320-5e75718fa6bd"
+# Define the new Power BI dashboard URL
+march_sales_dashboard_url = "https://app.powerbi.com/reportEmbed?reportId=9e25f385-722d-48c4-86f0-acccb34fc03a&autoAuth=true&ctid=d1aad15a-5724-45cd-a320-5e75718fa6bd"
 
-# HTML code for embedding Power BI dashboards
-daily_sales_html_code = f'<iframe title="تقرير المبيعات كاديت - اليومي" width="1140" height="541.25" src="{daily_sales_dashboard_url}" frameborder="0" allowFullScreen="true"></iframe>'
-overall_sales_html_code = f'<iframe title="تقرير المبيعات كاديت" width="1140" height="541.25" src="{overall_sales_dashboard_url}" frameborder="0" allowFullScreen="true"></iframe>'
+# HTML code for embedding the new Power BI dashboard
+march_sales_html_code = f'<iframe title="مارس" width="1140" height="541.25" src="{march_sales_dashboard_url}" frameborder="0" allowFullScreen="true"></iframe>'
 
-# Use Streamlit to display the Power BI dashboards using HTML in two rows
-st.markdown(daily_sales_html_code, unsafe_allow_html=True)
-st.markdown(overall_sales_html_code, unsafe_allow_html=True)
+# Use Streamlit to display the new Power BI dashboard using HTML
+st.markdown(march_sales_html_code, unsafe_allow_html=True)
 
 # Additional Streamlit app code can be added here
 
